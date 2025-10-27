1836f126c3ddafeba458fec1e69f02873ba138ad
ian40k <groupanimators4@gmail.com>
2025-10-27 23:14:05 +0300

    Create layout.js for WhatsApp Bot application

---
+++ sec/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'WhatsApp Bot - DOGMEN Pairing',
+  description: 'Connect to WhatsApp Bot with pairing code DOGMEN',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
